792c56f525cfdae92c0c240acf3a32d7dae72d7a
Ryazan-Amyderov <AndrewTechnologin@mail.ru>
2025-09-22 20:11:31 +0700

    Fix - movement direction, camera rotation

--- Assets/Scripts/Player/Movement/MovementModel.cs
+++ Assets/Scripts/Player/Movement/MovementModel.cs
@@ -9,18 +9,44 @@ namespace Player.Movement
         private float acceleration;
         private float maxSpeed;
         private float jumpForce;
+        private Transform cameraTransform;
 
-        public MovementModel(Rigidbody rb, MovementSettings settings)
+        public MovementModel(Rigidbody rb, MovementSettings settings, Transform cameraTransform)
         {
             _rb = rb;
             acceleration = settings.Acceleration;
             maxSpeed = settings.MaxSpeed;
             jumpForce = settings.JumpForce;
+            this.cameraTransform = cameraTransform;
         }
 
         public void Move(Vector2 direction)
         {
-            _rb.AddForce(new Vector3(direction.x, 0, direction.y).normalized * acceleration, ForceMode.Acceleration);
+            // ѕолучаем оси направлени€ с камеры (с обнулением Y!)
+            Vector3 camForward = cameraTransform.forward;
+            camForward.y = 0;
+            camForward.Normalize();
+            Vector3 camRight = cameraTransform.right;
+            camRight.y = 0;
+            camRight.Normalize();
+
+            Vector3 moveDir = camForward * direction.y + camRight * direction.x;
+            moveDir.Normalize();
+
+            _rb.AddForce(moveDir * acceleration, ForceMode.Acceleration);
+            // ѕосле применени€ силы Ч ограничим скорость!
+            LimitSpeed();
+        }
+
+        private void LimitSpeed()
+        {
+            Vector3 horizontalVel = _rb.linearVelocity;
+            horizontalVel.y = 0;
+            if (horizontalVel.magnitude > maxSpeed)
+            {
+                Vector3 clampedVel = horizontalVel.normalized * maxSpeed;
+                _rb.linearVelocity = new Vector3(clampedVel.x, _rb.linearVelocity.y, clampedVel.z);
+            }
         }
 
         public void Jump(InputAction.CallbackContext context)
